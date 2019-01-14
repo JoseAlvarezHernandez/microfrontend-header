849796544ea93875b6a523e1374e7e2e7a50d6c2
Jose Alvarez <alvarez_3993@hotmail.com>
2019-01-14 17:21:28 -0600

    fix(server): change server port

--- server.js
+++ server.js
@@ -4,6 +4,4 @@ var app = express();
 
 app.use('/', express.static('docs'));
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
-});
+app.listen(process.env.port || process.env.PORT || 3000);
